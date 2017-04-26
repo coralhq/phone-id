431b658157207114b5e03aac6f196ce15563bd3b
Batista Harahap <batista@bango29.com>
2017-04-26 16:15:25 +0700

    add files to register to pypi

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'phoneid',
+  packages = ['phoneid'],
+  version = '0.1',
+  description = 'A library to generate Indonesian phone numbers',
+  author = 'Batista Harahap',
+  author_email = 'batista@bango29.com',
+  url = 'https://github.com/coralhq/phone-id',
+  download_url = 'hhttps://github.com/coralhq/phone-id/archive/0.1.tar.gz',
+  keywords = ['phone', 'number', 'indonesia', 'telco'],
+  classifiers = [],
+)
